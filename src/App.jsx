2cd6b718ff4d18d79e29a3386b1f6745ea7ff8a2
dilshadbasith <dilshadbasith01@gmail.com>
2023-10-18 15:10:14 -0700

    update css

--- src/App.jsx
+++ src/App.jsx
@@ -31,10 +31,10 @@ function App() {
       <button onClick={addItem} className="button">ADD</button>
 
       <ul>
-        {items.map((item)=>{
-          return <li key={item.id} className="list">
-            {item.value}
-            <button className="dlt" onClick={()=>deleteItem(item.id)}>❌</button>
+        {items.map((b)=>{
+          return <li key={b.id} className="list">
+            {b.value}&nbsp;&nbsp;
+            <button className="dlt" onClick={()=>deleteItem(b.id)}>❌</button>
           </li>
         })}
       </ul>
